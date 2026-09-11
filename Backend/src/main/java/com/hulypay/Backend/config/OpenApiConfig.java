ca8f4493a28c785b1848f519ac5f2da2467015e0
Sujith S <sujith.sappani@gmail.com>
2026-09-11 22:14:04 +0530

    Swagger

---
+++ Backend/src/main/java/com/hulypay/Backend/config/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package com.hulypay.backend.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Huly.Pay API Documentation")
+                        .version("1.0.0")
+                        .description("REST API endpoints for Huly.Pay backend services"))
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth", new SecurityScheme()
+                                .name("BearerAuth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+
+}
