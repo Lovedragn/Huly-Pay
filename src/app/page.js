bcea0b543309ded1b02aafc216559cc5d8e2dadc
Sujith S <sujith.sappani@gmail.com>
2026-09-22 10:19:11 +0530

    footer

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import Footer from "@/components/landing/Footer";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-white">
+    <main className="min-h-screen bg-white w-full overflow-x-hidden">
       <Navbar />
       <Hero />
       <Workflow />
